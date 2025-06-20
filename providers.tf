e50ce5b4297039e9205001b4c0e11261e6096681
Francesco Martini <fra.martini@reply.it>
2025-06-20 10:30:08 +0200

    test su feature

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+
+  required_version = "~> 1.9.0"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.1"
+    }
+  }
+}
+
+provider "local" {}
--- terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-organization = {
-  domain = "TEST"
-  id     = "testtest"
-}
