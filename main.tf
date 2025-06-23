09a8860231878133783a4085dcaea29652d35861
Francesco Martini <fra.martini@reply.it>
2025-06-23 10:58:28 +0200

    removed resource test

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
-resource "local_file" "foo" {
-  content  = "foo!"
-  filename = "${path.module}/foo.bar"
-}
+#resource "local_file" "foo" {
+#  content  = "foo!"
+#  filename = "${path.module}/foo.bar"
+#}
 
 output "name" {
   value = var.environment
